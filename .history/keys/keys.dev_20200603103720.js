b0db636ab8445637536e43c2fca170ed47e54b29
Gregory Pievski <gregpiev@gmail.com>
2020-06-03 10:45:44 +0300

    3.06.2020_1

---
+++ .history/keys/keys.dev_20200603103720.js
@@ -0,0 +1,8 @@
+module.exports = {
+    password: 'piev180457',
+    MONGODB_URI: `mongodb+srv://gregpiev:piev180457@cluster0-lo4jr.mongodb.net/shop`,
+    SESSION_SECRET: 'some secret values',
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'gregpiev@gmail.com',
+    BASE_URL: 'http://localhost:3000'
+}
